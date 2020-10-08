62094055cd56cc4760795f6631685bc0c14798fd
ivanlohvyn <i.logvyn@gmail.com>
2020-10-08 17:00:54 +0000

    add tag list, create & detail in urls

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("post_list", views.post_list, name="post_list"),
     path("post_create/", views.post_create, name="post_create"),
     path("<str:slug>/", views.post_detail, name="post_detail"),
+    path("tag_list", views.tag_list, name="tag_list"),
+    path("<str:slug>/tag_detail", views.tag_detail, name="tag_detail"),
+    path("tag_create", views.tag_create, name="tag_create"),
 ]
