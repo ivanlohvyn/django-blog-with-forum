5b170ab403df3074ff24ca19f49370018730539f
ivanlohvyn <i.logvyn@gmail.com>
2020-10-08 14:22:50 +0000

    update views and url  with post create

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index"),
     path("post_list", views.post_list, name="post_list"),
+    path("post_create/", views.post_create, name="post_create"),
     path("<str:slug>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
 
 from .models import Post, Tag
+from .forms import PostForm
 
 
 def index(request):
@@ -15,6 +16,17 @@ def post_list(request):
         "blog/post_list.html",
         context={"latest_post_list": latest_post_list},
     )
+
+
+def post_create(request):
+    form = PostForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save()
+            return HttpResponseRedirect(reverse("blog:post_detail", args=(post.slug,)))
+    else:
+        form = PostForm()
+    return render(request, "blog/post_create.html", {"form": form})
 
 
 def post_detail(request, slug):
