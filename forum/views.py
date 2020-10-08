d5b7064698ed57e375de43db87e28e532a3afbb5
ivanlohvyn <i.logvyn@gmail.com>
2020-10-08 18:30:54 +0000

    add topic list & detail in views

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
+
+from .models import Topic, Message
+
+
+def topic_list(request):
+    latest_topic_list = Topic.objects.order_by("-created")[:10]
+    return render(
+        request,
+        "forum/topic_list.html",
+        context={"latest_topic_list": latest_topic_list},
+    )
+
+
+def topic_detail(request, slug):
+    topic = get_object_or_404(Topic, slug__iexact=slug)
+    message_list = topic.message_set.order_by("pub_date")
+    return render(
+        request,
+        "forum/topic_detail.html",
+        context={"topic": topic, "message_list": message_list},
+    )
