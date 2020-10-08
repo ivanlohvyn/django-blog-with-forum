ee9ee96bfe2ef971dce575eba39a1c8835de7e2e
ivanlohvyn <i.logvyn@gmail.com>
2020-10-08 13:54:22 +0000

    add post_list & post_detail in views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import Post, Tag
+
+
+def index(request):
+    return render(request, "blog/index.html")
+
+
+def post_list(request):
+    latest_post_list = Post.objects.order_by("-created")[:10]
+    return render(
+        request,
+        "blog/post_list.html",
+        context={"latest_post_list": latest_post_list},
+    )
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug__iexact=slug)
+    return render(request, "blog/post_detail.html", context={"post": post})
