1f13db6bc86cac66dd811d7c221f0d234fcaa903
ivanlohvyn <i.logvyn@gmail.com>
2020-10-08 16:59:50 +0000

    add TagForm in forms

--- blog/forms.py
+++ blog/forms.py
@@ -13,3 +13,15 @@ class PostForm(forms.ModelForm):
         if new_slug == "create":
             raise ValidationError("Slug may not be 'create'")
         return new_slug
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["title", "slug"]
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data["slug"].lower()
+        if new_slug == "create":
+            raise ValidationError("Slug may not be 'create'")
+        return new_slug
