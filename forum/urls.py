b14ef47d48b77e9dc27ac6a4ee7c0443c4daacdc
ivanlohvyn <i.logvyn@gmail.com>
2020-10-12 07:05:22 +0000

    add send messge url

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,8 @@ app_name = "forum"
 urlpatterns = [
     path("topic_list", views.topic_list, name="topic_list"),
     path("topic_create/", views.topic_create, name="topic_create"),
-    path("<str:slug>/topic_detail/", views.topic_detail, name="topic_detail"),
+    path("<str:slug>/", views.topic_detail, name="topic_detail"),
+    path(
+        "<str:slug>/send_message/", views.send_message, name="send_message"
+    ),
 ]
