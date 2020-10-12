7491a93d093732161d422f9ed055006646af3859
ivanlohvyn <i.logvyn@gmail.com>
2020-10-12 08:26:38 +0000

    add like message url

--- forum/urls.py
+++ forum/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("topic_list", views.topic_list, name="topic_list"),
     path("topic_create/", views.topic_create, name="topic_create"),
     path("<str:slug>/", views.topic_detail, name="topic_detail"),
-    path(
-        "<str:slug>/send_message/", views.send_message, name="send_message"
-    ),
+    path("<str:slug>/send_message/", views.send_message, name="send_message"),
+    path("like_message", views.like_message, name="like_message"),
 ]
