6c5bee55ac6ca183805b19d9203450e26c225efb
ivanlohvyn <i.logvyn@gmail.com>
2020-10-12 07:04:56 +0000

    add message form

--- forum/forms.py
+++ forum/forms.py
@@ -1,17 +1,22 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Topic
+from .models import Message, Topic
 
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         user = forms.ModelMultipleChoiceField(queryset=None)
-        fields = ["author", "title", "tags", "slug", "status"]
+        fields = ["title", "tags", "slug", "status"]
 
     def clean_slug(self):
         new_slug = self.cleaned_data["slug"].lower()
         if new_slug == "create":
             raise ValidationError("Slug may not be 'create'")
         return new_slug
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['text']
